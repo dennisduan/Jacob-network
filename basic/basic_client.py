5339b962e275026e280ba916648db6be31d61e48
unknown <dennis.duan@gmail.com>
2019-10-16 10:12:21 +0800

    Add AF_INET and SOCK_STREAM to avoid issue on MAC

--- basic/basic_client.py
+++ basic/basic_client.py
@@ -2,7 +2,7 @@
 
 import socket               # 导入 socket 模块
  
-s = socket.socket()         # 创建 socket 对象
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)         # 创建 socket 对象
 host = socket.gethostname() # 获取本地主机名
 port = 12345                # 设置端口号
  
--- basic/basic_server.py
+++ basic/basic_server.py
@@ -6,7 +6,7 @@ This is a simple socket server
 '''
 import socket               # 导入 socket 模块
 
-s = socket.socket()         # 创建 socket 对象
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)         # 创建 socket 对象
 host = socket.gethostname() # 获取本地主机名
 port = 12345                # 设置端口
 s.bind((host, port))        # 绑定端口
